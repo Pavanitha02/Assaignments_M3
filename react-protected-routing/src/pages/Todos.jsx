26b18b82ad22971860e5d181f7e751603be7864a
Pavanitha02 <kalasanivenkat764@gmail.com>
2025-12-25 18:34:59 +0530

    react-protected-routing

---
+++ react-protected-routing/src/pages/Todos.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data.slice(0, 10)));
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h2>Todos</h2>
+      <button onClick={logout}>Logout</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(`/todos/${todo.id}`)}
+          >
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
